5b9d9a33052547df660c5d08f110d0b75c2a6e55
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-03 14:42:49 +0700

    add popper.js in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,15 @@
+import "bootstrap/dist/css/bootstrap.css";
 import React, { useEffect } from "react";
 import "../styles/custom.css";
 import "font-awesome/css/font-awesome.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import popper
+import "popper.js";
 
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => { 
     import("bootstrap/dist/js/bootstrap.js");
   }, []);
+
   return (
     <>
       <Component {...pageProps} />;
