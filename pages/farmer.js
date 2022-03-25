37ecded64bcdf835c8a5d2060f1fbd90c355818d
Naufal Ibrahim <naufal.ti@outlook.com>
2022-03-25 14:57:48 +0700

    add axios

--- pages/farmer.js
+++ pages/farmer.js
@@ -1,14 +1,26 @@
 import ShowBarn from "@components/Barn/ShowBarn";
 import Navbar from "@components/Navbar";
 import React from "react";
+import axios from "axios";
+import Head from "next/head";
+import { headers } from "next.config";
+import { useState, useEffect } from "react";
 
 export default function farmer() {
+  const [barns, setBarns] = React.useState([]);
+  
+  useEffect(() => {
+        axios
+          .get("http://localhost:8181/api/farmer/barn/")
+          .then((res) => setBarns(res.data));
+      }, []);
+
   return (
     <div>
       <Navbar />
       <div className="container">
-      <ShowBarn />
-      farmer
+          <p>{barns.map(barn => barn.id)}</p>
+        <ShowBarn />
       </div>
     </div>
   );
