9529776f2ccc089bc0d164f550b26eeacad532bc
MITRAIS\Khafido_I343 <khafido.work@gmail.com>
2022-03-25 09:40:59 +0700

    add manager pages

---
+++ pages/manager/freezer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Freezer(){
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Freezer</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ pages/manager/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '@components/Navbar';
+
+export default function Index(){
+    return(
+        <div className="container">
+            <Navbar />
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Index</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
