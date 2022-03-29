1944dccb396d1dd79cda29dc5811d9e31401f270
arel <marcellinus.arel@mitrais.com>
2022-03-29 16:57:30 +0700

    add create item

--- components/Merchant/Merchant.js
+++ components/Merchant/Merchant.js
@@ -4,37 +4,47 @@ export default function Merchant() {
     const router = useRouter();
     return (
         <body>
-        <style jsx>{`
+            <style jsx>{`
         .btn {
             margin-left: 10px;
             margin-top: 10px;
             margin-bottom: 10px;
         }
         `}</style>
-        <div className="container">
-            <section className="features py-3">
-                <div className="container mx-auto sm: px-4 search-box py-3">
-                    <div className="flex flex-wrap">
-                        <div className="text-center md:w-full px-4">
-                            <button
-                                onClick={() => router.push("/merchant/bazaar")}
-                                className="btn btn-carrot radius-5"
-                            >
-                                {" "}
-                                Bazaar
-                            </button>
-                            <button
-                                onClick={() => router.push("/merchant/staffgroup")}
-                                className="btn btn-carrot radius-5"
-                            >
-                                {" "}
-                                Staff Group
-                            </button>
+            <div className="container">
+                <section className="features py-3">
+                    <div className="container mx-auto sm: px-4 search-box py-3">
+                        <div className="flex flex-wrap">
+                            <div className="text-center md:w-full px-4">
+                                <button
+                                    onClick={() => router.push("/merchant/bazaar")}
+                                    className="btn btn-carrot radius-5"
+                                >
+                                    {" "}
+                                    Bazaar
+                                </button>
+
+                                <button
+                                    onClick={() => router.push("/merchant/bazaaritem")}
+                                    className="btn btn-carrot radius-5"
+                                >
+                                    {" "}
+                                    Bazaar Item
+                                </button>
+
+                                <button
+                                    onClick={() => router.push("/merchant/staffgroup")}
+                                    className="btn btn-carrot radius-5"
+                                >
+                                    {" "}
+                                    Staff Group
+                                </button>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            </div>
         </body>
     )
 }
