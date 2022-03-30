9916222b0ccabe681c7fb54db8c560ab4fc9415e
Naufal Ibrahim <naufal.ti@outlook.com>
2022-03-30 13:57:16 +0700

    Barn History added

--- components/Barn/BarnHistory.js
+++ components/Barn/BarnHistory.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import History from "./History";
 
-export default function BarnHistory() {
+export default function BarnHistory(props) {
+  const [transfers, setTransfers] = useState([])
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8181/api/farmer/transfer/"+props.barnId.id)
+      .then((res) => setTransfers(res.data));
+  }, []);
+
+  
   return (
-    <div>BarnHistory WIP</div>
-  )
+    <>
+      <div>BarnHistory WIP</div>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Shared to</th>
+            <th>Amount</th>
+            <th>Date</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transfers.map((item, index) => {
+            return <History item={item} index={index} />
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
