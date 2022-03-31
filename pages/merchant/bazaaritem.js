1cd1af9685fc50346c14afbce755ff1aba914203
arel <marcellinus.arel@mitrais.com>
2022-03-31 09:51:40 +0700

    add item image feature

--- pages/merchant/bazaaritem.js
+++ pages/merchant/bazaaritem.js
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
 import CreateItem from "./features/createItem";
+import UpdateBazaarImage from "./features/addItemPicture";
 
 export default function BazaarItem() {
     const router = useRouter();
@@ -15,6 +16,8 @@ export default function BazaarItem() {
     const url = 'http://localhost:8181/api/bazaar/item'
     const [bazaarItem, setBazaarItem] = useState([]);
     const [showCreateItem, setShowCreateItem] = useState(false);
+    const [showUpdateItemImage, setShowUpdateItemImage] = useState(false);
+    const [id, setId] = useState(0);
 
     useEffect(() => {
         axios.get(url).then(response => setBazaarItem(response.data));
@@ -77,11 +80,17 @@ export default function BazaarItem() {
                                     <td>{data.quantity}</td>
                                     <td>{data.description}</td>
                                     <td>
-                                        <button type="button" className="btn border-blue-600 mr-2">
+                                        <button type="button" className="btn border-blue-600 mr-2"
+                                            onClick={() => {
+                                                setShowUpdateItemImage(true);
+                                                setId(data.id)
+                                            }}
+                                        >
                                             <i className="fa fa-edit text-blue-600 fa-x px-1">
 
                                             </i>
                                         </button>
+                                        {showUpdateItemImage && <UpdateBazaarImage closeClick={setShowUpdateItemImage} updateData={id} refreshPage={reloadPage} />}
                                     </td>
                                 </tr>
                             ))}
