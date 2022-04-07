f69dd223a1ac880016c1a83d38f4d0590db65c73
Naufal Ibrahim <naufal.ti@outlook.com>
2022-04-07 12:32:28 +0700

    give id to login button

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -139,6 +139,7 @@ const SignIn = ({ authenticate, auth, error, token }) => {
                       <div className="text-center mt-6">
                         <button
                           type="button"
+                          id="login-button"
                           className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
                           style={{ transition: "all .15s ease" }}
                           onClick={() => signIn()}
