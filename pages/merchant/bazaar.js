dcbdd5b5019d843604194035a94a4d73f44fb77a
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-04-12 11:59:40 +0700

    disable es lint next line

--- pages/merchant/bazaar.js
+++ pages/merchant/bazaar.js
@@ -12,92 +12,120 @@ import CreateBazaar from "./features/createBazaar";
 import UpdateBazaar from "./features/updateBazaar";
 
 export default function Bazaar() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const url = process.env.NEXT_PUBLIC_API_URL + 'bazaar'
-    const [bazaars, setBazaar] = useState([]);
-    const [showCreateBazaar, setShowCreateBazaar] = useState(false);
-    const [showUpdateBazaar, setShowUpdateBazaar] = useState(false);
-    const [selectedBazaar, setSelectedBazaar] = useState({});
+  const url = process.env.NEXT_PUBLIC_API_URL + "bazaar";
+  const [bazaars, setBazaar] = useState([]);
+  const [showCreateBazaar, setShowCreateBazaar] = useState(false);
+  const [showUpdateBazaar, setShowUpdateBazaar] = useState(false);
+  const [selectedBazaar, setSelectedBazaar] = useState({});
 
-    useEffect(() => {
-        axios.get(url).then(response => setBazaar(response.data));
-    }, [])
-    // console.log(bazaar)
+  useEffect(() => {
+    axios.get(url).then((response) => setBazaar(response.data));
+    //eslint-disable-next-line
+  }, []);
+  // console.log(bazaar)
 
+  function reloadPage() {
+    window.location.reload();
+  }
 
-
-
-    function reloadPage() {
-        window.location.reload();
-    }
-
-    return (
-        <body>
-            <Head />
-            <Navbar />
-            <Merchant />
-            <div className="container">
-                <section className="bazaar-table">
-                    <div className="row d-flex px-10">
-                        <h2 className="col-md-6 mt-4 pl-0 text-grey ml-0 mb-2">Bazaar Dashboard</h2>
-                    </div>
-                    <div className="container mx-auto sm: px-4 search-box py-3">
-                        <div className="row d-flex px-4">
-                            <h2 className="col-md-6 mt-4 pl-0 text-grey ml-0">Bazaar List</h2>
-                            <div className="col-md-6">
-                                <button
-                                    className="col-sm-6 btn btn-info mt-4 pull-right radius-5"
-                                    onClick={() => { setShowCreateBazaar(true) }}>
-                                    {" "}
-                                    Create New Bazaar
-                                </button>
-                                {showCreateBazaar && <CreateBazaar closeClick={setShowCreateBazaar} refreshPage={reloadPage} />}
-                            </div>
-                        </div>
-                        <table className="table table-hover mt-3">
-                            <thead>
-                                <tr>
-                                    <th itemScope="col" aria-rowspan={2}>#</th>
-                                    <th itemScope="col" aria-rowspan={2} style={{ display: "none" }}>id</th>
-                                    <th itemScope="col" aria-rowspan={2}>Bazaar Name</th>
-                                    <th itemScope="col" aria-rowspan={2}>Start Date</th>
-                                    <th itemScope="col" aria-rowspan={2}>End Date</th>
-                                    <th itemScope="col" aria-rowspan={2}>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {bazaars.map((data, index) => (
-                                    <tr key={index + 1}>
-                                        <td>{index + 1}</td>
-                                        <td style={{ display: "none" }}>{data.id}</td>
-                                        <td>{data.bazaarName}</td>
-                                        <td>{data.startDate}</td>
-                                        <td>{data.endDate}</td>
-                                        <td>
-                                            <button type="button" className="btn border-blue-600 mr-2"
-                                                onClick={() => {
-                                                    setShowUpdateBazaar(true);
-                                                    setSelectedBazaar(data);
-                                                    console.log(selectedBazaar);
-                                                }}
-                                            >
-                                                <i className="fa fa-edit text-blue-600 fa-x px-1">
-
-                                                </i>
-                                            </button>
-                                            {showUpdateBazaar && <UpdateBazaar closeClick={setShowUpdateBazaar} updateData={selectedBazaar} refreshPage={reloadPage} />}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-
-                        </table>
-                    </div>
-                </section>
-
+  return (
+    <body>
+      <Head />
+      <Navbar />
+      <Merchant />
+      <div className="container">
+        <section className="bazaar-table">
+          <div className="row d-flex px-10">
+            <h2 className="col-md-6 mt-4 pl-0 text-grey ml-0 mb-2">
+              Bazaar Dashboard
+            </h2>
+          </div>
+          <div className="container mx-auto sm: px-4 search-box py-3">
+            <div className="row d-flex px-4">
+              <h2 className="col-md-6 mt-4 pl-0 text-grey ml-0">Bazaar List</h2>
+              <div className="col-md-6">
+                <button
+                  className="col-sm-6 btn btn-info mt-4 pull-right radius-5"
+                  onClick={() => {
+                    setShowCreateBazaar(true);
+                  }}
+                >
+                  {" "}
+                  Create New Bazaar
+                </button>
+                {showCreateBazaar && (
+                  <CreateBazaar
+                    closeClick={setShowCreateBazaar}
+                    refreshPage={reloadPage}
+                  />
+                )}
+              </div>
             </div>
-            <Footer />
-        </body>
-    )
+            <table className="table table-hover mt-3">
+              <thead>
+                <tr>
+                  <th itemScope="col" aria-rowspan={2}>
+                    #
+                  </th>
+                  <th
+                    itemScope="col"
+                    aria-rowspan={2}
+                    style={{ display: "none" }}
+                  >
+                    id
+                  </th>
+                  <th itemScope="col" aria-rowspan={2}>
+                    Bazaar Name
+                  </th>
+                  <th itemScope="col" aria-rowspan={2}>
+                    Start Date
+                  </th>
+                  <th itemScope="col" aria-rowspan={2}>
+                    End Date
+                  </th>
+                  <th itemScope="col" aria-rowspan={2}>
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {bazaars.map((data, index) => (
+                  <tr key={index + 1}>
+                    <td>{index + 1}</td>
+                    <td style={{ display: "none" }}>{data.id}</td>
+                    <td>{data.bazaarName}</td>
+                    <td>{data.startDate}</td>
+                    <td>{data.endDate}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn border-blue-600 mr-2"
+                        onClick={() => {
+                          setShowUpdateBazaar(true);
+                          setSelectedBazaar(data);
+                          console.log(selectedBazaar);
+                        }}
+                      >
+                        <i className="fa fa-edit text-blue-600 fa-x px-1"></i>
+                      </button>
+                      {showUpdateBazaar && (
+                        <UpdateBazaar
+                          closeClick={setShowUpdateBazaar}
+                          updateData={selectedBazaar}
+                          refreshPage={reloadPage}
+                        />
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </body>
+  );
 }
