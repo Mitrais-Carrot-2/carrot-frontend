02bba60abf706152ccf640b1e39c06c33c84eabc
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-04-19 09:22:23 +0700

    edit dashboard style

--- components/employee/MiniBasketCards.js
+++ components/employee/MiniBasketCards.js
@@ -13,32 +13,43 @@ export default function MiniBasketCards(props) {
     state.user.userImage ? state.user.userImage : defaultImage
   );
 
-  function carrotCardBody(typeTxt, carrot){
+  function carrotCardBody(typeTxt, carrot) {
     let carrotTxt = "";
-    
-    if (props.basket){
-        if (carrot > 0) carrotTxt = `+${carrot}`
-        else carrotTxt = carrot
-    }
-    else carrotTxt = "";
+
+    if (props.basket) {
+      if (carrot > 0) carrotTxt = `+${carrot}`;
+      else carrotTxt = carrot;
+    } else carrotTxt = "";
 
     return (
-      <>  
+      <>
         <p className="font-bold uppercase text-[12px]">{typeTxt}</p>
         <h2>{carrotTxt} Carrots</h2>
       </>
-    )
+    );
   }
 
   return (
     <section>
       <div className="mx-auto my-4">
         <div className="grid grid-cols-3 gap-3 text-white mini-card">
-          <MiniCard cardType="mini-card-reward" image={carrotIcon} body={carrotCardBody("reward", props.basket.rewardCarrot)} />
-          <MiniCard cardType="mini-card-shared" image={carrotIcon} body={carrotCardBody("share", props.basket.shareCarrot)} />
-          <MiniCard cardType="mini-card-bazaar" image={carrotIcon} body={carrotCardBody("bazaar", props.basket.bazaarCarrot)} />
+          <MiniCard
+            cardType="mini-card-reward"
+            image={carrotIcon}
+            body={carrotCardBody("reward", props.basket.rewardCarrot)}
+          />
+          <MiniCard
+            cardType="mini-card-shared"
+            image={carrotIcon}
+            body={carrotCardBody("share", props.basket.shareCarrot)}
+          />
+          <MiniCard
+            cardType="mini-card-bazaar"
+            image={carrotIcon}
+            body={carrotCardBody("bazaar", props.basket.bazaarCarrot)}
+          />
         </div>
       </div>
-   </section>
-  )
+    </section>
+  );
 }
--- components/employee/MiniCard.js
+++ components/employee/MiniCard.js
@@ -1,26 +1,30 @@
 import React from "react";
 import Image from "next/image";
 
-export default function MiniCard(props) {  
+export default function MiniCard(props) {
   const imgWidth = 90;
   const imgHeight = 90;
 
-    return (
-        <div className={"w-full p-4 flex flex-row items-center rounded " + props.cardType} >
-            <div className="my-2">
-                <Image
-                    className={"h-auto w-full rounded-full " + props.imgClass}
-                    alt="user-profile"
-                    src={props.image}
-                    width={imgWidth}
-                    height={imgHeight}
-                    objectFit="cover"
-                    onClick={props.clickAction}
-                />
-            </div>
-        <div className="ml-8">
-            {props.body}
-        </div>
+  return (
+    <div
+      className={
+        "w-full p-4 flex flex-col items-center rounded " + props.cardType
+      }
+    >
+      <div className="my-2">
+        <Image
+          className={"h-auto w-full rounded-full " + props.imgClass}
+          alt="user-profile"
+          src={props.image}
+          width={imgWidth}
+          height={imgHeight}
+          objectFit="cover"
+          onClick={props.clickAction}
+        />
       </div>
-    )
+      <div className="flex flex-col justify-center text-center">
+        {props.body}
+      </div>
+    </div>
+  );
 }
